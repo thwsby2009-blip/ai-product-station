a5232a157d0aba9a41b0ad2104658933bad0b63b
thwsby2009-blip <thwsby2009@gmail.com>
2026-05-11 23:57:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="AI 課程")
 
 # 1️⃣ options 一定要先
 options = {
-    "0. 帳號註冊": "lesson_0",
+    "0. AI 產品設計全流程工作站": "lesson_0",
     "1. ChatGPT": "lesson_1",
     "2.4/20 Pandas課程": "lesson_420",
     "3.4/27 AI整合": "lesson_427",
