ec101d6e97ffa305ab82c306b1e2a7057b77477c
thwsby2009-blip <thwsby2009@gmail.com>
2026-05-12 00:57:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="AI 課程")
 options = {
     "0. AI 產品設計全流程工作站": "lesson_0",
     "1. AI 數據分析": "lesson_1",
-    "2.4/20 Pandas課程": "lesson_420",
+    "2. 金流數據分析": "lesson_420",
     "3.4/27 AI整合": "lesson_427",
     "4.Google Colab 雲端程式開發": "colab"
 }
